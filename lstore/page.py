91f6e0a31deb4942837feee7793647f6043ad89b
Nicholas Pinero <138171851+pizzaguy2004@users.noreply.github.com>
2026-01-27 19:24:36 -0800

    Update page.py
    Adds capacity to initialized page
    
    Check for determining if number of records is less than capacity
    
    Checks if capacity is full before adding data to database

--- lstore/page.py
+++ lstore/page.py
@@ -1,14 +1,18 @@
 
 class Page:
 
-    def __init__(self):
-        self.num_records = 0
+    def __init__(self, capacity):
+        self.num_records = capacity # Nicholas
         self.data = bytearray(4096)
 
     def has_capacity(self):
+        return self.num_records > len(self.data) # Nicholas
         pass
 
     def write(self, value):
-        self.num_records += 1
-        pass
+        if self.has_capacity() is True: # Nicholas
+            self.num_records += 1
+            pass
+        else:
+            pass
 
