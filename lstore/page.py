72fe396a38f78942e1e9f038d5e3bac691d6c9b2
Nicholas Pinero <138171851+pizzaguy2004@users.noreply.github.com>
2026-01-27 19:35:18 -0800

    Fixed capacity and data value on initialization
    Fixed capacity in initialized page code. Initialized data value as 0 in initialized page.

--- lstore/page.py
+++ lstore/page.py
@@ -2,11 +2,12 @@
 class Page:
 
     def __init__(self, capacity):
-        self.num_records = capacity # Nicholas
+        self.num_records = 0 # Nicholas
         self.data = bytearray(4096)
+        self.capacity = capacity
 
     def has_capacity(self):
-        return self.num_records > len(self.data) # Nicholas
+        return self.num_records < len(self.data) # Nicholas
         pass
 
     def write(self, value):
