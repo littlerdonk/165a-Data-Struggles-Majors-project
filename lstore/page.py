7f929d0547743f0646ce56bbac810a550530f443
Sage Dillemuth <sagedillemuth@gmail.com>
2026-01-31 12:06:52 -0800

    Optimized Page init and implemented write function
    Updated the Page class to support the default config files capacity and implemented the write method.

--- lstore/page.py
+++ lstore/page.py
@@ -1,19 +1,30 @@
 
 class Page:
 
-    def __init__(self, capacity):
+    def __init__(self, capacity = None):
         self.num_records = 0 # Nicholas
-        self.data = bytearray(4096)
-        self.capacity = capacity
+        self.data = bytearray(PAGE_SIZE)
+        if capacity = None: # Sage 
+            self.capacity = PAGE_SIZE // RECORD_SIZE # 512 records for a 4kb page
+        else: 
+            self.capacity = capacity
 
     def has_capacity(self):
         return self.num_records < self.capacity # Nicholas
-        pass
 
+    
     def write(self, value):
         if self.has_capacity() is True: # Nicholas
-            self.num_records += 1
-            pass
+            #Calculates offset like standard Lstore 
+            offset = self.num_records * RECORD_SIZE # Sage
+            if value is None: 
+                value = 0
+                #stores data as a 64-bit integer
+            self.data[offset + RECORD_SIZE] = value.to_bytes(RECORD_SIZE, byteorder='big', signed=True)
+            self.num_records += 1  # Nicholas
+            return offset 
         else:
-            pass
+            return -1
+            
+    def read()
 
