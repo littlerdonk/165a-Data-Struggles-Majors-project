ab85e43c15a6a36e5009c2f06c6ce76108f3b105
Nicholas Pinero <138171851+pizzaguy2004@users.noreply.github.com>
2026-02-15 15:55:21 -0800

    fixed sytax issues in buffer_insert and mark_dirty
    Replaced self.pool[-1] with my oldest_value variable so that code grabs actual oldest value from pool. Added key to initialization of mark_dirty().

--- lstore/bufferpool.py
+++ lstore/bufferpool.py
@@ -4,43 +4,42 @@ class BufferPool():
         self.pool = {}
         self.buffer_capacity = capacity
         self.dirty = set()
-    
-    def buffer_insert(self, key, value): # Nicholas
-        if key not in self.pool: #checks if requested key is already in buffer pool
+
+    def buffer_insert(self, key, value):  # Nicholas
+        if key not in self.pool:  # checks if requested key is already in buffer pool
             if self.buffer_at_capacity():
-                oldest_value = list(self.pool.keys())[0] #grabs oldest key from bufferpool for eviction
+                oldest_value = list(self.pool.keys())[0]  # grabs oldest key from bufferpool for eviction
                 for i in self.pool:
-                    if i not in drive: #checks if data is dirty
-                        self.mark_dirty(value) #Marks data in buffer pool as dirty if not in storage drive
-                     else:
+                    if i not in drive:  # checks if data is dirty
+                        self.mark_dirty(value)  # Marks data in buffer pool as dirty if not in storage drive
+                    else:
                         pass
-                if self.pool[-1] is dirty #checks if oldest value in buffer pool is dirty
+                if oldest_value in self.dirty:  # checks if oldest value in buffer pool is dirty
                     # Put code here to put dirty value in storage drive
-                    self.evict(self.pool[-1]) #evicts data from pool after writing to storage
-                    self.pool[-1] = key
+                    self.evict(oldest_value)  # evicts data from pool after writing to storage
+                    self.pool[key] = value
                 else:
-                    self.evict(self.pool) #evicts data from pool after writing to storage
-                    self.pool[-1] = key
-            else: 
-                self.pool[-1] = key
-                    
-                    
+                    self.evict(oldest_value)  # evicts data from pool after writing to storage
+                    self.pool[key] = value
+            else:
+                self.pool[key] = value
+
+
 
         elif key in self.pool:
-            existing_value = self.pool[key] #grabs value from pool
+            existing_value = self.pool[key]  # grabs value from pool
 
-
-
-    def buffer_at_capacity(self): # Nicholas
-        return len(self.pool) >= self.buffer_capacity #checks if there is capacity available in bufferpool
+    def buffer_at_capacity(self):  # Nicholas
+        return len(self.pool) >= self.buffer_capacity  # checks if there is capacity available in bufferpool
 
     def evict(self, key):
-        del self.pool[key] # simple delete from buffer pool but we might have to change this later to add more to it
+        del self.pool[key]  # simple delete from buffer pool but we might have to change this later to add more to it
         pass
 
-    def mark_dirty(self):
+    def mark_dirty(self, key):
         self.dirty.add(key)
         pass
+
 
     
 
