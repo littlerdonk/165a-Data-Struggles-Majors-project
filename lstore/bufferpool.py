df783768703c752f84c9d1b5b59941a6c8ef8f60
Nicholas Pinero <138171851+pizzaguy2004@users.noreply.github.com>
2026-02-15 16:09:59 -0800

    Adds comments and clarifies functio names

--- lstore/bufferpool.py
+++ lstore/bufferpool.py
@@ -5,6 +5,12 @@ class BufferPool():
         self.buffer_capacity = capacity
         self.dirty = set()
 
+    def buffer_at_capacity(self):  # Nicholas
+        return len(self.pool) >= self.buffer_capacity  # checks if there is capacity available in bufferpool and returns true/false
+
+    def evict_key(self, key):
+        del self.pool[key]  # deletes key from buffer pool
+
     def buffer_insert(self, key, value):  # Nicholas
         if key not in self.pool:  # checks if requested key is already in buffer pool
             if self.buffer_at_capacity():
@@ -16,10 +22,10 @@ class BufferPool():
                         pass
                 if oldest_value in self.dirty:  # checks if oldest value in buffer pool is dirty
                     # Put code here to put dirty value in storage drive
-                    self.evict(oldest_value)  # evicts data from pool after writing to storage
+                    self.evict_key(oldest_value)  # evicts data from pool after writing it to storage
                     self.pool[key] = value
                 else:
-                    self.evict(oldest_value)  # evicts data from pool after writing to storage
+                    self.evict_key(oldest_value)  # evicts non-dirty data from pool
                     self.pool[key] = value
             else:
                 self.pool[key] = value
@@ -29,16 +35,8 @@ class BufferPool():
         elif key in self.pool:
             existing_value = self.pool[key]  # grabs value from pool
 
-    def buffer_at_capacity(self):  # Nicholas
-        return len(self.pool) >= self.buffer_capacity  # checks if there is capacity available in bufferpool
-
-    def evict(self, key):
-        del self.pool[key]  # simple delete from buffer pool but we might have to change this later to add more to it
-        pass
-
     def mark_dirty(self, key):
-        self.dirty.add(key)
-        pass
+        self.dirty.add(key) #adds key to dirty value's set
 
 
     
