66a13200e0cf99b110d2fa3110a06baa1409ac08
Nicholas Pinero <138171851+pizzaguy2004@users.noreply.github.com>
2026-02-21 02:11:18 -0800

    Renamed variables, removed for loop, added placeholder for writing to storage, and added return if key already in buffer pool

--- lstore/bufferpool.py
+++ lstore/bufferpool.py
@@ -8,18 +8,15 @@ class BufferPool():
     def buffer_insert(self, key, value):  # Nicholas
         if key not in self.pool:  # checks if requested key is already in buffer pool and only moves forward if key is not in buffer pool
             if self.buffer_at_capacity(): # if bufferpool is at capacity then we must replace our oldest value with a new one
-                oldest_value = list(self.pool.keys())[0]  # grabs oldest key from bufferpool for eviction
-                for i in self.pool: # Goes through all the data in the buffer pool and marks the values that havent been written to the drive as dirty
-                    if i not in drive: # will have to find some way to check if updated data is already in the storage drive
-                        self.mark_dirty(value)  # Marks data in buffer pool as dirty if not in storage drive
-                    else:
-                        pass
-                if oldest_value in self.dirty: # If the oldest value in the buffer pool is not written to the secondary storage then we need to write it before eviction
+                oldest_key = list(self.pool.keys())[0]  # grabs oldest key from bufferpool for eviction
+                if oldest_key in self.dirty: # If the oldest value in the buffer pool is not written to the secondary storage then we need to write it before eviction
                     # Put code here to put dirty value in storage drive
-                    self.evict_key(oldest_value)  # evicts data from pool after writing it to storage
+                    self.drive[oldest_key] = self.pool[oldest_key] # self.drive doesnt exist yet but we'll add it later
+                    self.dirty.remove(oldest_key)
+                    self.evict_key(oldest_key)  # evicts data from pool after writing it to storage
                     self.pool[key] = value
                 else: # If oldest value in buffer pool is already written to secondary storage then it is safe to evict data from buffer pool
-                    self.evict_key(oldest_value)  # evicts non-dirty data from pool
+                    self.evict_key(oldest_key)  # evicts non-dirty data from pool
                     self.pool[key] = value
             else: # If buffer pool not at capacity then it safe to add new value without the need for any evictions
                 self.pool[key] = value
@@ -28,6 +25,7 @@ class BufferPool():
 
         elif key in self.pool: # if requested key is already in the buffer pool (RAM) then we simply grab that value
             existing_value = self.pool[key]  # grabs value from pool
+            return existing_value
 
     def mark_dirty(self, key):
         self.dirty.add(key) #adds key to dirty value's set
