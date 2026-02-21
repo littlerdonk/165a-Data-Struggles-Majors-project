f6a16bcb45c7a453673dc2de277d6d5c312a43aa
pigeon1234567890 <144746823+pigeon1234567890@users.noreply.github.com>
2026-02-21 02:51:47 -0800

    Update query.py
    Finished the rest of update cases that i meant to solve from earlier

--- lstore/query.py
+++ lstore/query.py
@@ -188,10 +188,20 @@ class Query:
                             self.table.index.insert_btree(i, columns[i], tailRID)
                     return True
                 else:
-                    self.table.index.insert_btree(self.table.key, primary_key, rid) # if its not updating ie updating = None roll back to avoid data corruption 
+                    #self.table.index.insert_btree(self.table.key, primary_key, rid) # if its not updating ie updating = None roll back to avoid data corruption 
+                    for i in range(0, len(old_version_info)):
+                        if self.table.index.indices[i] is not None:
+                            self.table.index.insert_btree(i, old_version_info[i], rid) 
                     return False
             else: #primary key not changing push normal update
+                old_version_info = self.table.get_record(rid).columns
+                for i in range(0, len(columns)):
+                    self.table.index.delete_rid(i, old_version_info[i], rid)
                 updating = self.table.update(rid, list(columns))
+                tailRID = self.table.rid - 1
+                for i in range(0, len(columns)):
+                        if self.table.index.indices[i] is not None:
+                            self.table.index.insert_btree(i, columns[i], tailRID)
                 return updating
             
         except:
