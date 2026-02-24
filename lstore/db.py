022bbf2e968dfca198329b450f1cf4aa66f58955
Sage Dillemuth <sagedillemuth@gmail.com>
2026-02-23 18:42:35 -0800

    Fix indentation for base page range check

--- lstore/db.py
+++ lstore/db.py
@@ -67,8 +67,8 @@ class Database():
                 for col in range(table.total_columns):
                     table.bufferpool.get_page(table.name, 'tail', range_index, col)
                         # make sure at least 1 base page range
-                        if table.cur_base_range_index < 0:
-                            table.new_base_page_range()
+                    if table.cur_base_range_index < 0:
+                        table.new_base_page_range()
                 
             #Sage: bug fixed logic below ?
             for base_rid, location in table.page_directory.items():
